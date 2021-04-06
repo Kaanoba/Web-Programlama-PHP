0abe95e8ca044a7d6da4cc459471ae1bb47cdf33
kaan <kaanoba@hotmail.com>
2021-04-06 12:27:40 +0300

    Php ders 1  kalan notlar yazıldı.

--- php_ders1.php
+++ php_ders1.php
@@ -41,7 +41,123 @@
 
     
     ?>
+    <?php
+    echo "<h3>Matematiksel İşlemler</h3> ";
 
+    echo "<h4>Toplama İşlemi</h4> ";
+    
+    $sayi1=15;
+    $sayi2=5;
+    echo "İşlem sonucu: $sayi1+$sayi2=";
+    echo $sayi1+$sayi2;
+
+    echo "<br> ";
+
+    $sonuc=$sayi1+$sayi2;
+    echo "<br> ";
+    echo "$sayi1+$sayi2=$sonuc";
+
+
+    echo "<h4>Çıkarma İşlemi</h4> ";
+    
+    $sayi1=15;
+    $sayi2=5;
+    echo "İşlem sonucu: $sayi1-$sayi2=";
+    echo $sayi1-$sayi2;
+
+    echo "<br> ";
+
+    $sonuc=$sayi1-$sayi2;
+    echo "<br> ";
+    echo "$sayi1-$sayi2=$sonuc";
+
+
+    echo "<h4>Çarpma İşlemi</h4> ";
+    
+    $sayi1=15;
+    $sayi2=5;
+    echo "İşlem sonucu: $sayi1*$sayi2=";
+    echo $sayi1*$sayi2;
+
+    echo "<br> ";
+
+    $sonuc=$sayi1*$sayi2;
+    echo "<br> ";
+    echo "$sayi1*$sayi2=$sonuc";
+
+
+    echo "<h4>Bölme İşlemi</h4> ";
+    
+    $sayi1=15;
+    $sayi2=5;
+    echo "İşlem sonucu: $sayi1/$sayi2=";
+    echo $sayi1/$sayi2;
+
+    echo "<br> ";
+
+    $sonuc=$sayi1/$sayi2;
+    echo "<br> ";
+    echo "$sayi1/$sayi2=$sonuc";
+
+
+
+    // Çift tırnak ve tek tırnak farkı 
+    echo "<h3>Tırnaklar arasındaki fark</h3> ";
+    $oyun="The Witcher 3";
+    echo "$oyun ile Geralt a Rivia."; // Çift tırnak ile değişken içeriği okunur.
+    echo "<br>";
+    echo '$oyun \'u bu sene bitireceğim'; // tek tırnak değişkenin kendisini yazar.
+
+
+    echo "<h3>Atama Operatörleri.</h3> ";
+
+    $sayi=300;
+    echo "\$sayi değişkeni içerisindeki değer: $sayi <br>";
+
+    $sayi += 150; // $sayi= $sayi + 150
+    echo "\$sayi değişkeni içerisindeki değer: $sayi <br>";
+    
+    $sayi -= 50; 
+    echo "\$sayi değişkeni içerisindeki değer: $sayi <br>";
+    
+    $sayi /= 5; // $sayi = $sayi / 5
+    echo "\$sayi değişkeni içerisindeki değer: $sayi <br>";
+   
+    $sayi *= 15; 
+    echo "\$sayi değişkeni içerisindeki değer: $sayi <br>";
+    
+    $sayi++ ;  // $sayi= $sayi + 1
+    echo "\$sayi değişkeni içerisindeki değer: $sayi <br>";
+
+    $sayi-- ;  
+    echo "\$sayi değişkeni içerisindeki değer: $sayi <br>";
+   
+    ++$sayi ;  
+    echo "\$sayi değişkeni içerisindeki değer: $sayi <br>";
+
+    --$sayi;  
+    echo "\$sayi değişkeni içerisindeki değer: $sayi <br>";
+
+
+
+    echo "<h3>Hazır Fonksiyonlar</h3> ";
+
+    echo "<h5> Rand </h5> "; // Rasgele sayı üretir.
+
+    $sayi= rand(0,100);
+    echo "\$sayi içerisindeki rasgele üretilen değer: $sayi <br>";
+
+    // Örnek; rasgele 0-100 arasında üretilen değer 50 den küçükse kaldı, değilse geçti yazsın
+
+    if ($sayi >= 50) {
+        echo "geçti- notunuz : $sayi";
+    } else {
+        echo "Kaldı- notunuz: $sayi";
+    }
+
+
+
+    ?>
 
 
 
